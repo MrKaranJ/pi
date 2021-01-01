b1f6b840323661e941d295fdac191255e3a353c6
Karan Jayachandra <mail@karanjayachandra.com>
2021-01-01 21:37:29 +0100

    Added help to the function and changed the code to round the return value

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Pi was originally defined as the ratio of the circumference of a circle to its d
 
 ![Archimedes Formula](/images/archimedes.png)
 
-As n increase, the polygon will be a better approximate of a circle and will we get a better estimate of Pi.
+As n increases, the polygon will be a better approximate of a circle and will we get a better estimate of Pi.
 
 Here a simple program has been written to calculate Pi using the [**Bailey–Borwein–Plouffe**](https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula) (BBP) formula. Which is defined as shown below:
 
 ![BBP Formula](/images/bbp.png)
+
+To use the program. Use the defined function 'getpi' in main.py with an argument N for the precision required. With N = 100, the function returns Pi upto 100 decimal places. Run main.py to check the function.
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+# Load the decimal package to reconfigure the defauly precision 
+# of the decimal type as float doesn't have the capabilities
 from decimal import getcontext, Decimal
 
 def getpi(n):
-    print(3.14)
+    """
+    Take input 'n' and returns Pi upto n values.
+
+    USAGE:
+
+    print(getpi(4))
+
+    OUTPUT:
+
+    3.1415
+    """
+    # Set precision to be two decimals more than required
     getcontext().prec = n + 2
+    # Initialize variable to zero for the loop
     pi = 0
+    # Calculate each iteration of the BBP expansion
     for i in range(n):
         t1 = Decimal(4 / (8*i + 1))
         t2 = Decimal(2 / (8*i + 4))
@@ -11,12 +26,15 @@ def getpi(n):
         t4 = Decimal(1 / (8*i + 6))
         iteration = Decimal((t1 - t2 - t3 - t4) / 16**i)
         pi = pi + iteration
-    print(pi)
+    return round(pi, n)
 
+# Take input for the precision requirement
 n = input("Enter precision required: \n")
+# Set precision based on input
 precision = int(n)
+# Check the validity of the input and test the function
 if isinstance(precision, int):
-    print("Precision Required: {0}".format(precision))
-    getpi(precision)
+    print("\nPrecision Required: {0}".format(precision))
+    print(getpi(precision))
 else:
     print("Couldn't parse entered value. Try again.")
